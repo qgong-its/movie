534752946b2f229166f3c16b4b4eaa40e4217220
Qian Gong <gongqian.de@gmail.com>
2026-03-27 21:44:18 +0100

    feat(main): initialize popular movies view and add/remove functionality

--- src/main.js
+++ src/main.js
@@ -1,4 +1,37 @@
 import './style.css';
-import { createHomePage } from './pages/homePage.js';
 
-document.querySelector('#app').innerHTML = createHomePage();
+import { fetchPopularMovies, searchMovies } from './modules/network.js';
+import { renderMovieCard, setupBtn } from './modules/ui.js';
+import { getFromLocalStorage, saveToLocalStorage } from './modules/storage.js';
+
+async function init() {
+  try {
+    const movies = await fetchPopularMovies();
+    // console.log(movies);
+    renderMovieCard(movies, 'card-container');
+    // const movie = await searchMovies('Project Hail Mary');
+    // console.log(movie);
+    // renderMovieCard(movie.results, 'movie-container');
+  } catch (error) {
+    console.log('failed to fetch movies: ', error);
+  }
+
+  setupBtn('card-container', ({ type, movieId }) => {
+    const favourite = getFromLocalStorage('favourite');
+
+    if (type === 'add') {
+      if (!favourite.includes(movieId)) {
+        favourite.unshift(movieId);
+        saveToLocalStorage('favourite', favourite);
+      }
+    } else if (type === 'remove') {
+      const index = favourite.indexOf(movieId);
+      if (index !== -1) {
+        favourite.splice(index, 1);
+        saveToLocalStorage('favourite', favourite);
+      }
+    }
+  });
+}
+
+init();
